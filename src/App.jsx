fb884fea0c17c9d3ce311e86bb7d0b458bfb9784
Bhammond9 <142949554+Bhammond901@users.noreply.github.com>
2025-01-16 15:13:38 -0700

    some cool commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@
 import './App.css';
 import Counter from './Counter';
 import ScoreKeeper from "./ScoreKeeper";
+import EmojiClicker from "./EmojiClicker";
 
 function App() {
   
 
   return (
     <>
-    <h1>State Demo!</h1>
-    <Counter/>
+    {/* <h1>State Demo!</h1>}
+    // <ScoreKeeper/> */}
+    <EmojiClicker/>
+    {/* <Counter/> */}
     
     </>
   );
 }
 
-export default App
+export default App;
